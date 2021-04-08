0a5d97d91798eb285a0046bbc7f4fe9d969589eb
scrubele <mankoania@gmail.com>
2021-04-08 16:10:45 +0200

    Added gym refactoring.

--- evaluate.py
+++ evaluate.py
@@ -527,7 +527,10 @@ def mem_scheme_su_evaluate(input_settings, layer_, im2col_layer, layer_index, la
                                                              layer_comb, spatial_loop_comb, ii_su)
 
     if RL_search_engine and not (input_settings.fixed_temporal_mapping or loma_search_engine):
-        return rl_temporal_mapping_optimizer(None, layer_, layer_post, im2col_layer, layer_rounded, spatial_loop_comb,
+        print("layer_", layer_)
+        print("layer_post", layer_post)
+        print("size_list_output_print", layer_.size_list_output_print)
+        return rl_temporal_mapping_optimizer(None, layer_, im2col_layer, layer_rounded, spatial_loop_comb,
                                              input_settings,
                                              mem_scheme, ii_su)
 
