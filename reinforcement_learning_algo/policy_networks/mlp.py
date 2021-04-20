6270817c145ef69fea26eb158195a736cbf16dd9
scrubele <mankoania@gmail.com>
2021-04-20 15:11:33 +0200

    Updated requirements.

--- reinforcement_learning_algo/policy_networks/mlp.py
+++ reinforcement_learning_algo/policy_networks/mlp.py
@@ -1,12 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-
+from torch import Tensor
 
 class MLP(nn.Module):
     def __init__(self, observation_space_length, action_space_length):
-
         super(MLP, self).__init__()
 
         self.action_space_length = action_space_length
@@ -17,8 +15,28 @@ class MLP(nn.Module):
         self.saved_log_probs = []
         self.rewards = []
 
+        self.path = "/home/scrubele/Documents/project/reinforcement-learning-zigzag/reinforcement_learning_algo/policy_networks/model.h5"
+
     def forward(self, x):
         x = F.relu(self.dropout(self.fc1(x)))
         action_scores = self.fc2(x)
         # return F.softmax(action_scores, dim=0)
         return action_scores
+
+    def save(self):
+        torch.save(self.state_dict(),
+                   self.path)
+
+    def load(self):
+        self.load_state_dict(torch.load(self.path))
+        self.eval()
+
+    # make a class prediction for one row of data
+    def predict(self, row, model):
+        # convert row to data
+        # row = Tensor(row)
+        # make prediction
+        yhat = model(row)
+        # retrieve numpy array
+        yhat = yhat.detach().numpy()
+        return yhat
